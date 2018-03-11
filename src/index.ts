1496e25bbe1ccca0ade1b08a42b37331844cf435
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-10 22:51:51 -0800

    add tabletop class stub

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hi");
+import * as $ from "jquery";
+import Tabletop from "./tabletop";
+
+const tabletop = new Tabletop($("#game") as JQuery<HTMLCanvasElement>);
+
+console.log(tabletop);
---
+++ src/tabletop/index.ts
@@ -0,0 +1,7 @@
+import * as $ from "jquery";
+
+export default class Tabletop {
+	constructor(private canvas: JQuery<HTMLCanvasElement>) {
+		console.log(canvas);
+	}
+}
