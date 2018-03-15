e207c2d6811e1aa2235b250a21d92fc5c7472ae1
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-14 17:06:17 -0700

    add card backs

--- src/card-game/dom-helper.ts
+++ src/card-game/dom-helper.ts
@@ -4,7 +4,8 @@ export class CardGameDomHelper extends DomHelper {
 	createCardFrag(cardImgUrl: string) {
 		let $card = $(`
 			<div class="card">
-				<img class="card__img" src="/assets/cards/${cardImgUrl}.svg">
+				<img class="card__front" src="/assets/cards/${cardImgUrl}.svg">
+				<img class="card__back" src="/assets/cards/card_back.png">
 			</div>
 		`);
 
