59771d9a40cb4573f0fe692095cd15f3e8398c7b
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-14 18:15:05 -0700

    fix copy webpack plugin again

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
 	plugins: [
 		new CopyWebpackPlugin([
 			{ from: "src/assets", to: "assets"},
-			{ from: "index.html", to: "docs"}
+			{ from: "index.html", to: "."}
 		])
 	],
 	devServer: {
