a683cf8121a475229291f0aa53ee3a46852f7c6a
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-15 12:44:10 -0700

    add docs for server connection

--- rewrite/server/server-connection.ts
+++ rewrite/server/server-connection.ts
@@ -5,9 +5,21 @@
 import "firebase";
 import { Action } from "./action";
 
+/**
+ * Handles communication with the Firebase server.
+ *
+ * You may extend this class for your custom game.
+ */
 export class ServerConnection {
+	/**
+	 * Array of listeners for events.
+	 */
 	protected listeners: { event: string, callback: Function }[] = [];
 
+	/**
+	 * Constructs a new instance of ServerConnection and starts listening for events.
+	 * @param gameId a unique game id used to identify the game.
+	 */
 	constructor(protected gameId: string) {
 		if (!firebase.apps.length) {
 			// Initialize Firebase
@@ -24,6 +36,10 @@ export class ServerConnection {
 		this.start();
 	}
 
+	/**
+	 * Starts listening for actions. Automatically called when
+	 * ServerConnection is initialized.
+	 */
 	start() {
 		firebase.database()
 			.ref(`/game/${this.gameId}/actions`)
@@ -35,6 +51,12 @@ export class ServerConnection {
 			});
 	}
 
+	/**
+	 * Runs through all previous actions.
+	 *
+	 * Useful when a user disconnects and reconnects.
+	 * Attach all listeners, then call this function.
+	 */
 	runPrevActions() {
 		return firebase.database()
 			.ref(`/game/${this.gameId}/actions`)
@@ -53,15 +75,28 @@ export class ServerConnection {
 			});
 	}
 
+	/**
+	 * Listen to an event
+	 * @param event The event name to listen to
+	 * @param callback A function to call when an action with the given event occurs
+	 */
 	on(event: string, callback: Function) {
 		this.listeners.push({ event, callback });
 	}
 
+	/**
+	 * Dispatch an action
+	 * @param action The action to dispatch
+	 */
 	dispatch(action: Action) {
 		action.timestamp = Date.now();
 		firebase.database().ref(`/game/${this.gameId}/actions`).push(action);
 	}
 
+	/**
+	 * Calls all listeners listening for the action
+	 * @param action The action that occurred
+	 */
 	protected handleAction(action: Action) {
 		this.listeners.forEach(listener => {
 			if (listener.event === action.event) {
