a818db65f43ecd2272808df11208c24214eedcff
Nathan Wang <nathan.r.wang@gmail.com>
2019-02-28 16:52:03 -0800

    add basic modal for bidding

--- tabletop/games/bridge/dom-helper.ts
+++ tabletop/games/bridge/dom-helper.ts
@@ -4,6 +4,48 @@
 import { CardGameDomHelper } from "../../card-game";
 
 export class BridgeDomHelper extends CardGameDomHelper {
+	protected $biddingModal: JQuery<HTMLElement>;
+
+	constructor(
+		protected $container: JQuery<HTMLElement>
+	) {
+		super($container);
+
+		this.$biddingModal = $(`
+			<div class="bridge-bidding-modal">
+				<h3>Bidding</h3>
+				<div class="bridge-bidding-modal__value">
+					<div class="btn-group">
+						<button type="button" class="btn btn-secondary">1</button>
+						<button type="button" class="btn btn-secondary">2</button>
+						<button type="button" class="btn btn-secondary">3</button>
+						<button type="button" class="btn btn-secondary">4</button>
+						<button type="button" class="btn btn-secondary">5</button>
+						<button type="button" class="btn btn-secondary">6</button>
+						<button type="button" class="btn btn-secondary">7</button>
+					</div>
+				</div>
+				<div class="bridge-bidding-modal__suit">
+					<div class="btn-group">
+						<button type="button" class="btn btn-secondary">Club</button>
+						<button type="button" class="btn btn-secondary">Diamond</button>
+						<button type="button" class="btn btn-secondary">Heart</button>
+						<button type="button" class="btn btn-secondary">Spade</button>
+					</div>
+				</div>
+				<div class="bridge-bidding-modal__past-bids">
+					<h4>Past Bids</h4>
+					<p>Player #1: 1 Heart</p>
+					<p>Player #2: 1 Heart</p>
+					<p>Player #3: 1 Heart</p>
+				</div>
+				<button type="button" class="btn btn-outline-primary btn-lg">Confirm</button>
+			</div>
+		`);
+
+		this.$frag.append(this.$biddingModal);
+	}
+
 	createBridgePlayerFrag(name: string, tricks: number, isTurn: boolean) {
 		let $player = $(`
 			<div class="player bridge-player ${isTurn ? 'bridge-player--active' : ''}">
